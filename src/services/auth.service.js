5a82965ad07e7ff36330b6c6a2ad5758ea41993b
MoisesDavila000 <m.ivan.dr.10@gmail.com>
2024-10-16 18:57:58 -0600

    fix: correcion de los imports y exports en authservice

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,19 +1,19 @@
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
 
 // Encriptar contraseña
-const encryptPassword = async (password) => {
+export const encryptPassword = async (password) => {
   const salt = await bcrypt.genSalt(10);
   return await bcrypt.hash(password, salt);
 };
 
 // Comparar contraseña
-const comparePassword = async (password, hashedPassword) => {
+export const comparePassword = async (password, hashedPassword) => {
   return await bcrypt.compare(password, hashedPassword);
 };
 
 // Generar JWT
-const generateToken = (userId) => {
+export const generateToken = (userId) => {
   const payload = { id: userId };
   const secret = process.env.JWT_SECRET || 'your_jwt_secret'; // Asegúrate de definir JWT_SECRET en tu archivo .env
   const options = { expiresIn: '1h' }; // Expira en 1 hora
@@ -21,18 +21,11 @@ const generateToken = (userId) => {
 };
 
 // Validar JWT
-const validateToken = (token) => {
+export const validateToken = (token) => {
   try {
     const secret = process.env.JWT_SECRET || 'your_jwt_secret';
     return jwt.verify(token, secret);
   } catch (error) {
     return null; // Token inválido
   }
-};
-
-module.exports = {
-  encryptPassword,
-  comparePassword,
-  generateToken,
-  validateToken
 };
