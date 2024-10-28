31744ba9e997ee470fbea2aeeb0d5ceca4f4715f
MoisesDavila000 <m.ivan.dr.10@gmail.com>
2024-10-28 14:35:24 -0600

    feat: Uso de CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 //configuracion de express
 import express from "express";
+import cors from "cors";
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 import adviseesRoutes from './routes/advisees.routes.js';
 import advisorsRoutes from './routes/advisors.routes.js';
@@ -8,7 +12,17 @@ import degreesRoutes from './routes/degrees.routes.js';
 import learningUnitRoutes from './routes/learningUnit.routes.js';
 import usersRoutes from './routes/users.routes.js';
 
+
 const app = express();
+
+const corsOptions = {
+    origin: process.env.FRONT_URL || 'http://localhost:4000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+};
+
+// Habilitar CORS
+app.use(cors(corsOptions));
 
 // Middlewares
 app.use(express.json());
