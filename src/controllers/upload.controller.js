b18b36aefe8f01335d87451eeecca8487605540b
b9Mike <migu31.z.a@gmail.com>
2024-11-01 15:34:01 -0600

    feat: Se Agrego la funcionalidad de subir archivos a drive

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,10 +1,15 @@
 import { uploadFile } from '../services/googleDrive.service.js';
 
 export const uploadDocumentToDrive = async (req, res) => {
-    const { filePath, fileName } = req.body; // Supongamos que recibes estos datos en el cuerpo de la petición
+    const { filePath, fileName } = req.body; // Recibimos el la ruta del excel y el nuevo nombre que tendra en el drive
+
+    if (!filePath || !fileName) {
+        console.log(filePath);
+        return res.status(400).json({ message: 'filePath y fileName son obligatorios' });
+    }
 
     try {
-        const fileData = await uploadFile(filePath, fileName);
+        const fileData = await uploadFile(filePath, fileName); // se lo mandamos a servies de google drive api
         res.status(200).json({ message: 'Archivo subido con éxito', fileData });
     } catch (error) {
         res.status(500).json({ message: 'Error al subir el archivo', error: error.message });
--- src/services/googleDrive.service.js
+++ src/services/googleDrive.service.js
@@ -11,30 +11,43 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 dotenv.config();
 
-//pa que jale bien el dir name
+//pa que jale bien el dir name lo definimos como arriba
 
 const auth = new google.auth.GoogleAuth({
-    keyFile: path.join(__dirname, '../../config/google-service-account.json'), // Las credenciales las pondremos en la carpeta raiz config
+    keyFile: path.join(__dirname, '../../config/departamento-asesorias-02a726e2e629.json'), // Las credenciales las pondremos en la carpeta raiz config
     scopes: ['https://www.googleapis.com/auth/drive.file'],
 });
 
 const drive = google.drive({ version: 'v3', auth });
 
 export const uploadFile = async (filePath, fileName) => {
+    const folderId = '1PheHop9DIOg5AjuKF47Zu0ymbKNG1wc4'; // Es el ID de la carpeta en drive la cual es "Reportes"
+
     try {
         const response = await drive.files.create({
             requestBody: {
                 name: fileName,
-                mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // MIME type para .xlsx excel
+                mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // MIME type para .xlsx
+                parents: [folderId], // Aqui pusimos el id de la carpeta para que se guarde justo ahí
             },
             media: {
-                mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // MIME type para .xlsx excel 
+                mimeType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', // MIME type para .xlsx 
                 body: fs.createReadStream(filePath),
             },
         });
+
+        await drive.permissions.create({
+            fileId: response.data.id,
+            requestBody: {
+                role: 'reader',
+                type: 'anyone',
+            },
+        });
+
         return response.data;
     } catch (error) {
         console.error('Error al subir el archivo a Google Drive:', error);
         throw new Error('No se pudo subir el archivo a Google Drive');
     }
 };
+
